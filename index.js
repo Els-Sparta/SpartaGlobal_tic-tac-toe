baa8429a549f63f5b8223a0a6a4747eb27aad117
Els <Emathivannan@spartaglobal.com>
2018-08-12 11:58:09 +0100

    Assigned clicked squares to that specifc player

--- index.js
+++ index.js
@@ -5,37 +5,52 @@ $(function(event){
   //the minimum possible amount fo turns it takes to win is 5
   var minWinningTurn = 5;
   //all the possible winning conditions
+  var playerCross = [];
+  var playerNought = [];
   var winningConditions = [[0, 1, 2], [3, 4, 5], [6, 7 ,8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]]
-
   //whenever a div with the class 'square' is clicked the 'this' stores data about it and assigns it a value.
   $(".square").each(function(index){
     $(this).data("square", index);
   })
   //This only allows a div to be clicked once and depending on the turn will denote that div as nought or cross
   $(".square").one("click", function(){
-    console.log($(this).data("square"));
+    $(this).data("square");
     //adds 1 to the value of turn each time there is a click
     turn++;
     //gives a variable to the current item clicked
     var currentTurn = $(this);
     //if the varaible had an even number it would be nought and odd would be cross
     if(turn % 2) {
-      currentTurn.addClass('nought');
+      //this changes the clicked elements class, so background-color
+      currentTurn.addClass('cross');
+      //this one adds an "x" to the element clicked
+      currentTurn.append("X")
+      //stores the number of the square clicked to that player
+      playerCross += currentTurn.data("square");
+      //logs the square numbers of that player
+      console.log(playerCross);
     }
     else{
-      currentTurn.addClass('cross');
+      //changes the background-color of the element clicked
+      currentTurn.addClass('nought');
+      //adds an "O" to the element clicked
+      currentTurn.append("O");
+      //stores the squareNumber clicked tot that specific player
+      playerNought += currentTurn.data("square");
+      //logs the square number of that player
+      console.log(playerNought);
     }
 
-    function playerWins(playerCross){
-      return winningCondition.some(function(threeInARow){
-        return threeInARow.every(function (square){
-          return board[square] === playerCross;
-
-        });
-      });
-
-      var xWins = playerWins('X');
-    }
+    // function playerWins(playerCross){
+    //   return winningCondition.some(function(threeInARow){
+    //     return threeInARow.every(function (square){
+    //       return board[square] === playerCross;
+    //
+    //     });
+    //   });
+    //
+    //   var xWins = playerWins('X')
+    // }
 
     // function winningCondition(){
     //
